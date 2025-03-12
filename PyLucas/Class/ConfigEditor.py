1034c047312163fd34c3d25bdfeaa966eca99fce
LuLuLuOfficial <xframilrainx@163.com>
2025-03-13 01:52:51 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   ChangeLog.txt
    	modified:   PyLucas/Class/ConfigEditor.py
    	new file:   dist/PyLucas-3.1.0.tar.gz

--- PyLucas/Class/ConfigEditor.py
+++ PyLucas/Class/ConfigEditor.py
@@ -42,6 +42,12 @@ class ConfigEditor():
             
 
     def Get_Value(self, Key_Locate: str):
+        '''
+        It should be noted that when using Get_Value(), there is still a reference relationship between the returned Temp_Data and Data_Tomml.
+        需要注意的是, 在使用 Get_Value() 时, 返回的 Temp_Data 与 Data_Toml 之间仍然存在引用关系.
+        Therefore, if you need to undo the reference relationship between Temp_Data and Data_Tomml, you need to perform deepcopy() operation on Temp_Data.
+        所以, 如果需要解除 Temp_Data 与 Data_Toml 之间的引用关系, 需要对 Temp_Data 进行 deepcopy() 操作.
+        '''
         Key_Locate: list = Key_Locate.split('.')
         Temp_Data: any = self.__Data_Toml
         for Temp_Key in Key_Locate:
@@ -51,8 +57,8 @@ class ConfigEditor():
 
     def Set_Value(self, Key_Locate: str, Value: any):
         '''
-        It should be noted that when using Set_Value(), the modification of the Value accepted by Set_Value() will still affect the variables that are set_Value().
-        需要注意的是, 在使用 Set_Value() 时, 对 Set_Value() 所接受的的 Value 的修改仍然会影响被Set_Value()的变量.
+        It should be noted that after using Set_Value(), there is still a reference relationship between the variable represented by Value and Data_Tomml.
+        需要注意的是, 在使用 Set_Value() 后, Value 所代表的变量与 Data_Toml 之间仍然存在引用关系.
         Therefore, if you need to undo the reference relationship between Value and Data_Tomml, you need to perform deepcopy() operation on the variable represented by Value.
         所以, 如果需要解除 Value 与 Data_Toml 之间的引用关系, 需要对 Value 所代表的变量进行 deepcopy() 操作.
         '''
