906efaec2ac60df2b1c24c35b2553f063b891212
LuLuLuOfficial <xframilrainx@163.com>
2025-03-12 18:33:05 +0800

    Changes to be committed: 	modified:   ChangeLog.txt 	modified:   PyLucas/Class/LogManager.py 	modified:   PyLucas/__MetaData__.py 	new file:   dist/PyLucas-3.0.0.tar.gz

--- PyLucas/Class/LogManager.py
+++ PyLucas/Class/LogManager.py
@@ -10,7 +10,7 @@ class LogManager():
         self.OutPutPath_Root: str = OutPutPath_Root
         self.OutPutPath_File: str = OutPutPath_Root
 
-        self.MassageLineBreak: bool = False
+        self.MessageLineBreak: bool = False
         self.LogLimit: list[bool, int] = [True, 10]
 
         self.Initialize()
@@ -32,9 +32,9 @@ class LogManager():
         if not Limit: self.LogLimit[0] = Mode
         self.LogLimit = [Mode, Limit]
 
-    def SetMassageLB(self, Mode: bool):
-        if Mode: self.MassageLineBreak = True
-        else: self.MassageLineBreak = False
+    def SetMessageLB(self, Mode: bool):
+        if Mode: self.MessageLineBreak = True
+        else: self.MessageLineBreak = False
 
     def CheckLogLimit(self):
         Path = _Path(self.OutPutPath_Root)
@@ -43,25 +43,25 @@ class LogManager():
             return
         while (self.LogLimit[0]) and (len(Files) > self.LogLimit[1]):
             OldestFile = min(Files, key=lambda f: f.stat().st_mtime)
-            self.LogOutput(LogMassage = f'Deleted Oldest LogFile -> {OldestFile}.')
+            self.LogOutput(LogMessage = f'Deleted Oldest LogFile -> {OldestFile}.')
             OldestFile.unlink()
             Files = [f for f in Path.iterdir() if f.is_file() and f.suffix.lower() == '.txt']
 
-    def LogOutput(self, Module: str = None, Level: str = 'Normal', LogMassage: str = 'Invalid Information', DoPrint: bool = True):
+    def LogOutput(self, Module: str = None, Level: str = 'Normal', LogMessage: str = 'Invalid Information', DoPrint: bool = True):
         '''
         Module: str = 'By Auto'
         Level: str = 'Error' | 'Warn' | 'Normal'
-        LogMassage: str = 'Invalid Information.'
+        LogMessage: str = 'Invalid Information.'
         DoPrint: bool = True | False
         '''
         if not Module: Module = stack()[1][0].f_globals['__name__']
         TimeStamp = GetTimeStamp()
         Indent: str = ''
-        if self.MassageLineBreak: Indent = '\n\t'
+        if self.MessageLineBreak: Indent = '\n\t'
         else: Indent = ' '
-        if LogMassage[-1] in ('.', '。',): LogMassage = LogMassage[:-1]
+        if LogMessage[-1] in ('.', '。',): LogMessage = LogMessage[:-1]
 
-        LogText: str = f'{TimeStamp} |-| [Level: <{Level}> | Module: <{Module}>]:{Indent}{LogMassage}.'
+        LogText: str = f'{TimeStamp} |-| [Level: <{Level}> | Module: <{Module}>]:{Indent}{LogMessage}.'
         
         if DoPrint:
             print(LogText)
