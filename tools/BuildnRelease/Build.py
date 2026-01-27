4dfebc2414b1be8fd1ba69406cb93614bff4971c
nuhil-lucas <nuhillucas@gmail.com>
2026-01-27 18:39:19 +0800

    migration commit

--- tools/BuildnRelease/Build.py
+++ tools/BuildnRelease/Build.py
@@ -2,4 +2,8 @@ from subprocess import run
 from os import getcwd
 
 if __name__ == "__main__":
-    run(args=[f"{getcwd().replace('\\', '/')}/tools/BuildnRelease/Build.bat"], cwd="./tools/BuildnRelease", shell=True)
+    run(
+        args=[f"{getcwd().replace('\\', '/')}/tools/BuildnRelease/Build.bat"],
+        cwd="./tools/BuildnRelease",
+        shell=True
+    )
